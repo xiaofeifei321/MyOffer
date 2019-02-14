513e0662525c04b6c51fea3de7069127fd999122
lijiongfei <1559101183@qq.com>
2019-02-14 09:41:15 +0800

    测试提交

--- src/com/ccut/offer/Problem13.java
+++ src/com/ccut/offer/Problem13.java
@@ -35,7 +35,7 @@ public class Problem13 {
 	}
 	public static void main(String[] args){
 		Problem13 test = new Problem13();
-		//qqqq
+		//
 		System.out.println(test.isEven(2));
 //		int[] arr= {1,2,3,4,5,6,12,7,8,9,10};
 //		test.order(arr);
