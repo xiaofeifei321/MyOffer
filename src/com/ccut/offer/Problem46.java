366a7c7c4c19d0129e6e6abaf96a6551239a7f83
ljf <874189630@qq.com>
2019-11-13 21:12:03 +0800

    1：求1+2+3+…+n，要求不能使用乘除法、 for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。 2：不用加减乘除做加法

--- src/com/ccut/offer/Problem46.java
+++ src/com/ccut/offer/Problem46.java
@@ -2,40 +2,31 @@ package com.ccut.offer;
 
 /**
  * 求1+2+3+…+n，要求不能使用乘除法、 for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
- * 解法一:
- *   用二进制位运算来求和
- *    1：第一步不考虑进位对每一位相加。0+0，1+1的结果都是0，1+0的结果是1。0+1的结果是1，我们注意到这和异或的结果是一样的。对异或而言，0和0，1和1异或的结果是0，而0和1，1和0的结果异或
- *    2：对0+0，0+1，1+0而言，都不会产生进位，只有1+1时，会向前差生一个进位。此时我们可以想象成是两个数先做位运算，然后再向前左移动一位。只有两个数都是1的时候
- *    3:相加的过程是重复前面的两步，直到不产生进位为止。
+ * 1:
+ *
  */
 public class Problem46 {
     public static void main(String[] args) {
         Problem46 solution46 = new Problem46();
         // System.out.println(solution46.Sum_Solution(3));
-        System.out.println(Problem46.add(5,21));
+        System.out.println(solution46.Sum_Solution_2(3));
     }
 
     /**
-     * 用异或运算求和
+     * 用自带的函数 （n^2+n）/2
      */
-    public static int add(int num1,int num2){
-        int sum,carry;
-        do{
-            sum = num1 ^ num2;
-            System.out.println(num1&num2);
-            carry = (num1&num2) <<1;
-            num1 = sum;
-            num2 = carry;
-        }while(num2!=0);
-        return num1;
+    int Sum_Solution3(int n) {
+        int result = (int)Math.pow(n,2)+n;
+        return result>>1;
     }
+
 
     /**
      * 用异常退出递归
      */
     public int Sum_Solution_2(int n) {
         try {
-            int i = 1 % n;
+            int i = 1 /n;
             return n + Sum_Solution_2(n - 1);
         } catch (Exception e) {
             return 0;
@@ -49,7 +40,7 @@ public class Problem46 {
      */
     public int Sum_Solution(int n) {
         int sum = n;
-        boolean flag = (sum > 0) && ((sum += Sum_Solution(n - 1)) > 0);
+        boolean flag = (sum > 0) && (((sum =sum+ Sum_Solution(n - 1))) > 0);
         return sum;
     }
 }
