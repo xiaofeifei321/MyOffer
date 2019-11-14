30c10b4b10adee63279b08af416f0a0b82b83c48
ljf <874189630@qq.com>
2019-11-14 10:35:31 +0800

    将字符串转化为数字

--- src/com/ccut/offer/Problem49.java
+++ src/com/ccut/offer/Problem49.java
@@ -1,7 +1,10 @@
 package com.ccut.offer;
 
 /**
- * 把字符串转换成整数
+ * 题目:讲一个字符串转换成一个整数(实现Integer.valueOf(string)的功能，但是string不符合数字要求时返回0)，要求不能使用字符串转换整数的库函数。 数值为0或者字符串不是一个合法的数值则返回0。
+ * 1：输入值是否合法，判断除首位可能为正负号外，其他位是否都是数字
+ * 2：中途中的非数字就报异常，0-9对应 48-57 A-Z对应 65-90 a-z对应 97-122，字符串-48对应的数字
+ * 3：一步步sum = sum * 10 + chars[i] - 48
  */
 public class Problem49 {
     //边界条件判断条件
@@ -52,7 +55,8 @@ public class Problem49 {
     }
 
     public static void main(String[] args) {
-
+        char a='1';
+        System.out.println(a);
         String str = "-123";
         System.out.println(new Problem49().StringToInt(str));
 
