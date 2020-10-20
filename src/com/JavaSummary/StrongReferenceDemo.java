651078d39f88277178f696089eb2f59a83947165
lijiongfei <lijiongfei@4kgarden.com>
2020-10-20 11:17:12 +0800

    java中强引用

--- src/com/JavaSummary/StrongReferenceDemo.java
+++ src/com/JavaSummary/StrongReferenceDemo.java
@@ -6,9 +6,12 @@ package com.JavaSummary;
 public class StrongReferenceDemo {
     public static void main(String[] args) {
         Object obj1 = new Object();//这样定义默认的就是强引用
-        Object obj2 = obj1;//obj2引用赋值
+        Object obj2 = obj1;//obj2引用赋值,指向刚刚创建的Object对象
+        //obj1=null;
         obj1 = null;
+        //进行垃圾回收
         System.gc();
+
         System.out.println(obj1);
         System.out.println(obj2);
     }
