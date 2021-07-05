2ab38970cb865266456490eb82db62d3c7843b9f
lijiongfei <lijiongfei@4kgarden.com>
2021-07-05 18:20:22 +0800

    2021/07/05 编辑距离

--- src/main/java/com/labuladong/dongtaiguihua/BianJiJuli.java
+++ src/main/java/com/labuladong/dongtaiguihua/BianJiJuli.java
@@ -37,8 +37,11 @@ public class BianJiJuli {
         // 计算所有 DP 值
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
+                //删除
                 int left = D[i - 1][j] + 1;
+                //插入，在s1[i]插入一个和s2[j]一样的字符.s2[j]被匹配，前移j，继续和i做对比
                 int down = D[i][j - 1] + 1;
+                //替换跳过
                 int left_down = D[i - 1][j - 1];
                 if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
                     left_down += 1;
@@ -47,5 +50,13 @@ public class BianJiJuli {
             }
         }
         return D[n][m];
+    }
+
+
+    public static void main(String[] args) {
+        BianJiJuli bianJiJuli = new BianJiJuli();
+        String a = "rad";
+        String b = "apple";
+        System.out.println(bianJiJuli.minDistance(a, b));
     }
 }
